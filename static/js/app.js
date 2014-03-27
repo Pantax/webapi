5dcf667b3073e9ecc859312bc1607a5c3710d99d
levoovoo <ozlevka@lev-ubuntu-desktop.(none)>
2014-03-27 13:16:15 +0200

    Use async it's cool Finish existing api. Start create new api functions with GUI

--- static/js/app.js
+++ static/js/app.js
@@ -14,7 +14,7 @@
 
             $('#btnPatients').click(function(){
                 $('#sideMenuContainer').empty().load('templates/patient_menu.html', function(){
-                    pageInstance.categoryInit();
+
                 });
             });
         },
@@ -23,11 +23,37 @@
             $('#mainDataContainer').empty().load('templates/category_form.html', function(){
                 wait(true);
                 runGetAjaxQuery(pageInstance.options.apiurl + '/getAllCategories', function(err, result){
-                    wait(false);
-                    if(err) {}
-                    else {
-                        var res = result;
+                    if(err) {
+                        wait(false);
                     }
+                    else {
+                        var results = $.parseJSON(result);
+                        async.each(results, function(category,callback){
+                            $('#cntCategories').append('<li>' + category.category_name + '</li>');
+                            callback(null);
+                        }, function(err){
+                            wait(false);
+                        });
+                    }
+                });
+
+                $('#frmCategory').submit(function()
+                {
+                    //todo check category name  to not empty
+                    wait(true);
+                    var category = {
+                        category_name : $('#txtCategoryName').val()
+                    };
+
+                    runPostAjaxQuery(pageInstance.options.apiurl + '/saveCategory', category, function(err, result){
+                        if(err) {}
+                        else {
+                            //todo check results
+                            $('#cntCategories').append('<li>' + category.category_name + '</li>');
+                            wait(false);
+                        }
+                    });
+                    return false;
                 });
             });
         }
@@ -37,14 +63,28 @@
         $.ajax({
             url : url,
             method : 'GET',
-            success : function(result) {
+            success : function(result, status) {
                 callback(null, result);
             },
             error : function(err) {
                 callback(err);
             }
-        })
+        });
     };
+
+    function runPostAjaxQuery(url, data, callback) {
+        $.ajax({
+            url : url,
+            method : 'POST',
+            data : data,
+            success : function(result, status) {
+                callback(null, result);
+            },
+            error : function(err) {
+                callback(err);
+            }
+        });
+    }
 
     function wait(show) {
         if(show)
